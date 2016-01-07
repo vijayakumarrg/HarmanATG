70d963012ecf0aeb9d7a139085afa759d231351a
vijayakumarrg <vijayakumarrg@aditi.com>
2016-01-07 09:30:41 +0530

    jspm added

---
+++ wwwroot/app/greeting.js
@@ -0,0 +1,16 @@
+﻿
+import React from 'react';
+
+var colors = ["#c0c0c0", "#ff0000", "#00ff00", "#0000ff"];
+
+class Greeting extends React.Component{
+    constructor(){
+        super();
+    }
+
+    render(){
+        return (<div>Hello there {this.props.name}!</div>);
+    }
+}
+
+export default Greeting;
---
+++ wwwroot/app/main.js
@@ -0,0 +1,9 @@
+﻿
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Greeting from './greeting.js';
+
+(() => {
+    ReactDOM.render(<Greeting name="Harman" />, document.getElementById('app'));
+})();
